e61bebd428637422b809faf95366f125b83a19c1
No miracle <nattaponpond.ch@gmail.com>
2022-09-11 16:54:52 +0700

    Add invest function

--- smartcontract/contracts/SmartFunding.sol
+++ smartcontract/contracts/SmartFunding.sol
@@ -1,10 +1,48 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.16;
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+// Reward = (totalSupply / goal) * input
 
 contract SmartFunding {
     address public tokenAddress;
+    uint public goal;
+    uint public pool;
+    uint public endTimeInDay;
+
+    mapping(address => uint256) public investOf;
+    mapping(address => uint256) public rewardOf;
+
+    event Invest(address indexed from, uint256 amount);
 
     constructor(address _tokenAddress) {
         tokenAddress = _tokenAddress;
+    }
+
+     modifier whenInvested () {
+         require(investOf[msg.sender] == 0, "Already invest");
+        _;
+    }
+
+     modifier whenInvestedZero () {
+         require(msg.value > 0, "Reject amount of invest");
+        _;
+    }
+
+    function initialize(uint _goal, uint _endTimeInDay) external {
+        goal = _goal;
+        endTimeInDay = block.timestamp + (_endTimeInDay * 1 days);
+    }
+
+    function invest () external payable whenInvested whenInvestedZero{
+        uint256 totalSupply = IERC20(tokenAddress).totalSupply();
+        uint256 rewardAmount = (totalSupply / goal) * msg.value;
+
+        investOf[msg.sender] = msg.value;
+        rewardOf[msg.sender] = rewardAmount;
+        pool += msg.value;
+
+        emit Invest(msg.sender, msg.value);
     }
 }
--- smartcontract/test/SmartFunding-test.js
+++ smartcontract/test/SmartFunding-test.js
@@ -2,27 +2,86 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const { utils } = ethers;
 
+const decimals = 18;
+
 describe("Deploy smart funding contract", function () {
-    it("Should deploy smartfunding", async function() {
-        const [owner] = await ethers.getSigners();
+    let owner;
+    let tokenContract;
+    let fundingContract;
+
+    beforeEach(async function() {
+        [owner] = await ethers.getSigners();
         const APPToken = await ethers.getContractFactory("APPToken");
-        const tokenContract = await APPToken.deploy();
+        tokenContract = await APPToken.deploy();
         await tokenContract.deployed();
 
         const SmartFundingContract = await ethers.getContractFactory("SmartFunding");
-        const fundingContract = await SmartFundingContract.deploy(tokenContract.address);
+        fundingContract = await SmartFundingContract.deploy(tokenContract.address);
         await fundingContract.deployed();
-        
-        // check correct address
-        expect(await fundingContract.tokenAddress()).to.equal(tokenContract.address);
-        // check correct supply
-        expect(await tokenContract.totalSupply()).to.equal(utils.parseUnits("1000000", 18));
-        // check owner address have correct token
-        expect(await tokenContract.balanceOf(owner.address)).to.equal(utils.parseUnits("1000000", 18));
-        
-        // check correct token after transfer
-        await tokenContract.connect(owner).transfer(fundingContract.address, utils.parseUnits("1000000", 18));
-        expect(await tokenContract.balanceOf(owner.address)).to.equal(utils.parseUnits("0", 18));
-        expect(await tokenContract.balanceOf(fundingContract.address)).to.equal(utils.parseUnits("1000000", 18));
     })
+        
+    it("Should correct & supply address", async function() {
+        // Should correct address
+        expect(await fundingContract.tokenAddress()).to.equal(tokenContract.address);
+        // Should correct supply
+        expect(await tokenContract.totalSupply()).to.equal(utils.parseUnits("1000000", decimals));
+    })
+
+    it("Should transfer owner token to smartfunding contract", async function() {
+        // Should owner address have correct token
+        expect(await tokenContract.balanceOf(owner.address)).to.equal(utils.parseUnits("1000000", decimals));
+        
+        // Transfer
+        await tokenContract.connect(owner).transfer(fundingContract.address, utils.parseUnits("1000000", decimals));
+        expect(await tokenContract.balanceOf(owner.address)).to.equal(utils.parseUnits("0", decimals));
+        expect(await tokenContract.balanceOf(fundingContract.address)).to.equal(utils.parseUnits("1000000", decimals));
+
+        // Initialize
+        await fundingContract.initialize(utils.parseEther("1"), 7);
+        expect(await fundingContract.goal()).to.equal(utils.parseEther("1"));
+    })
+})
+
+describe("SmartFunding operations", function () {
+    let owner;
+    let investor1;
+    let investor2;
+    let investor3;
+    let tokenContract;
+    let fundingContract;
+
+    beforeEach(async function() {
+        [owner, investor1, investor2, investor3] = await ethers.getSigners();
+        const APPToken = await ethers.getContractFactory("APPToken");
+        tokenContract = await APPToken.deploy();
+        await tokenContract.deployed();
+
+        const SmartFundingContract = await ethers.getContractFactory("SmartFunding");
+        fundingContract = await SmartFundingContract.deploy(tokenContract.address);
+        await fundingContract.deployed();
+
+        await tokenContract.connect(owner).transfer(fundingContract.address, utils.parseUnits("1000000", decimals));
+        await fundingContract.initialize(utils.parseEther("1"), 7);
+    })
+
+    it("Should invest success", async function() {
+        const tx = await fundingContract.connect(investor1).invest({value: utils.parseEther("0.1")});
+        await tx.wait();
+        const tx1 = await fundingContract.connect(investor2).invest({value: utils.parseEther("0.2")});
+        await tx1.wait();
+        const tx2 = fundingContract.connect(investor3).invest({value: utils.parseEther("0")});
+        await expect(tx2).to.be.revertedWith("Reject amount of invest");
+
+        expect(await fundingContract.pool()).to.equal(utils.parseEther("0.3"));
+        expect(await fundingContract.investOf(investor1.address)).to.equal(utils.parseEther("0.1"));
+        expect(await fundingContract.investOf(investor2.address)).to.equal(utils.parseEther("0.2"));
+        expect(tx).to.emit(fundingContract, "Invest").withArgs(investor1.address, utils.parseEther("0.1"));
+        expect(tx1).to.emit(fundingContract, "Invest").withArgs(investor2.address, utils.parseEther("0.2"));
+
+        expect(await fundingContract.rewardOf(investor1.address)).to.equals(utils.parseUnits("100000", decimals));
+        expect(await fundingContract.rewardOf(investor2.address)).to.equals(utils.parseUnits("200000", decimals));
+
+
+    })
+
 })
