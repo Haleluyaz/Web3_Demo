2636a367e46e38bb1191d66882a37039f7c1cec2
No miracle <nattaponpond.ch@gmail.com>
2022-10-06 21:55:38 +0700

    final backend

--- backend/index.js
+++ backend/index.js
@@ -8,16 +8,23 @@ const cors = require('cors')
 const port = 5000;
 const app = express();
 app.use(cors());
-
-app.get('/', async (req, res) => {
+app.use((req, res, next) => {
     const rawJson = fs.readFileSync(join('./', 'abi.json'), 'utf8');
     const abi = JSON.parse(rawJson);
     const provider = new ethers.providers.JsonRpcProvider(`${process.env.GORELI_URL}`, 5);
+    const signer = new ethers.Wallet(`${process.env.PRIVATE_KEY}`, provider);
     const contract = new ethers.Contract(
         `${process.env.CONTRACT_ADDRESS}`,
         abi,
-        provider)
-    
+        signer);
+
+    req.contract = contract;
+    next();
+})
+
+app.get('/', async (req, res) => {
+    const contract = req.contract;
+
     const goalTask = contract.goal().then(res => utils.formatEther(res));
     const poolTask = contract.pool().then(res => utils.formatEther(res));
     const endTimeTask = contract.endTime().then(res => res * 1000);
@@ -26,6 +33,18 @@ app.get('/', async (req, res) => {
     const progress =((pool / goal) * 100).toPrecision(4)
     
     res.json({goal, pool, endTime, progress});
+})
+
+app.get("/pause", async (req, res) => {
+    const contract = req.contract;
+    const tx = await contract.pause();
+    res.json({txHash: tx.hash})
+})
+
+app.get("/unpause", async (req, res) => {
+    const contract = req.contract;
+    const tx = await contract.unpause();
+    res.json({txHash: tx.hash})
 })
 
 app.listen(port, () => {
